fe6cdfe361dfdc1aebda3654e4de954e965af0e0
Jacob Castiglioni <jacobcastiglioni@Jacobs-MacBook-Pro.local>
2021-03-14 19:09:23 -0400

    almost there

--- src/services/dbConfig.js
+++ src/services/dbConfig.js
@@ -5,32 +5,22 @@ import 'firebase/firestore'
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyDQeMfG15IFXgDT_5RYOQferU7pds7E0Fc",
-    authDomain: "trivia-7a705.firebaseapp.com",
-    projectId: "trivia-7a705",
-    storageBucket: "trivia-7a705.appspot.com",
-    messagingSenderId: "775258286646",
-    appId: "1:775258286646:web:797eec688d5ffd16708697"
-  };
+  apiKey: "AIzaSyDQeMfG15IFXgDT_5RYOQferU7pds7E0Fc",
+  authDomain: "trivia-7a705.firebaseapp.com",
+  projectId: "trivia-7a705",
+  storageBucket: "trivia-7a705.appspot.com",
+  messagingSenderId: "775258286646",
+  appId: "1:775258286646:web:797eec688d5ffd16708697"
+};
+
   // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  
-  export const db = firebase.firestore()
-  export const auth = firebase.auth()
-  export const colRefTrivia = db.collection("trivia")
-  export const colRefQuestion = db.collection("question")
-  const docRefPanel = colRefTrivia.doc("Panel")
+firebase.initializeApp(firebaseConfig);
 
-
-
-  // const docRefBuzzUser = colRefTrivia.doc("Buzzer")
-  // const docRefBuzzStatus = colRefTrivia.doc("Bar")
-  // const docRefAnswerCard = colRefTrivia.doc("Answer")
-  // const docRefPopup = colRefTrivia.doc("Popup")
-  
-  
-  
-  
+export const db = firebase.firestore()
+export const auth = firebase.auth()
+export const colRefTrivia = db.collection("trivia")
+export const colRefQuestion = db.collection("question")
+const docRefPanel = colRefTrivia.doc("Panel")
 
   
 export const toggleQ = async (num) => {
@@ -41,113 +31,97 @@ export const toggleQ = async (num) => {
   )
 }
 
-// export const readPopup = (obs) => {
-//   return (
-//     docRefPanel.onSnapshot(obs)
-//   )
-// }
 
-// export const getPopup = async () => {
-//   return (
-//     await docRefPopup.get()
-//   )
-// }
+export const signInAnon = () => {
+  auth.setPersistence(firebase.auth.Auth.Persistence.NONE)
+  return (
+    auth.signInAnonymously()
+  )
+}
 
-// export const setPopup = async (val) => {
-//   return (
-//     await docRefPopup.update({
-//       showPopup: val
-//     })
-//   )
-// }
+export const signOut = () => {
+  return (
+    auth.signOut()
+  )
+}
 
-  export const signInAnon = () => {
-    auth.setPersistence(firebase.auth.Auth.Persistence.NONE)
-    return (
-      auth.signInAnonymously()
-      )
-  }
+export const getBuzzUser = async () => {
+  return (
+    await docRefPanel.get()
+  )
+}
 
-  export const signOut = () => {
-    return (
-      auth.signOut()
-    )
-  }
-
-  export const getBuzzUser = async () => {
-    return (
-      await docRefPanel.get()
-    )
-  }
-
-  export const setBuzzUser = async (name) => {
-    return (
-      await docRefPanel.update({
-        name: name,
-        buzzerStatus: true
-      })
-    )
-  }
-
-  export const resetBuzzUser = async () => {
-    return (
-      await docRefPanel.update({
-        name: "Buzzer Active",
-        buzzerStatus: false
-      })
-    )
-  }
-
-  export const readBuzzerStatus = (observer) => {
-    return (
+export const getButtonStatus = (observer) => {
+  return (
       docRefPanel.onSnapshot(observer)
-    )
-  }
+  )
+}
 
-  export const toggleBuzzBar = async () => {
-   return (
-     docRefPanel.update({
+export const getAnswerCard = (observer) => {
+  return (
+    docRefPanel.onSnapshot(observer)
+  )
+}
+
+export const readBuzzerStatus = (observer) => {
+  return (
+    docRefPanel.onSnapshot(observer)
+  )
+}
+
+export const setBuzzUser = async (name) => {
+  return (
+    await docRefPanel.update({
+      name: name,
+      buzzerStatus: true
+    })
+  )
+}
+
+export const resetBuzzUser = async () => {
+  return (
+    await docRefPanel.update({
+      name: "Buzzer Active",
+      buzzerStatus: false
+    })
+  )
+}
+
+
+
+export const toggleBuzzBar = async () => {
+  return (
+    docRefPanel.update({
       barStatus: !(await docRefPanel.get()).data().barStatus
-     })
-   )
-  }
+    })
+  )
+}
 
-  export const setBuzzBar = async (val) => {
-    return (
-      docRefPanel.update({
-        barStatus: val
-      })
-    )
-  }
-
-  export const getButtonStatus = (observer) => {
-    return (
-       docRefPanel.onSnapshot(observer)
-    )
-  }
+export const setBuzzBar = async (val) => {
+  return (
+    docRefPanel.update({
+      barStatus: val
+    })
+  )
+}
 
 
-  export const getAnswerCard = (observer) => {
-    return (
-      docRefPanel.onSnapshot(observer)
-    )
-  }
 
-  export const setAnswerCard = (val) => {
-    return (
-      docRefPanel.update({
-        answerCard: val
-      })
-    )
-  }
+export const setAnswerCard = (val) => {
+  return (
+    docRefPanel.update({
+      answerCard: val
+    })
+  )
+}
 
-  export const toggleAnswerCard = async () => {
-    return (
-      docRefPanel.update({
-        answerCard: ! (await docRefPanel.get()).data().answerCard
-      })
-    )
-  }
+export const toggleAnswerCard = async () => {
+  return (
+    docRefPanel.update({
+      answerCard: ! (await docRefPanel.get()).data().answerCard
+    })
+  )
+}
 
   
 
